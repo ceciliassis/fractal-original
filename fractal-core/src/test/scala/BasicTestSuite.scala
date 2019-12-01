4d0411bc7b409010d10a55ac97081e93f64a9250
Cecília Assis <cy_assis@hotmail.com>
2019-12-01 04:48:02 -0300

    Add wracc filter support

--- fractal-core/src/test/scala/BasicTestSuite.scala
+++ fractal-core/src/test/scala/BasicTestSuite.scala
@@ -1,5 +1,7 @@
 package br.ufmg.cs.systems.fractal
 
+import br.ufmg.cs.systems.fractal.computation.Computation
+import br.ufmg.cs.systems.fractal.subgraph.{EdgeInducedSubgraph, VertexInducedSubgraph}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Tag}
 
@@ -13,6 +15,9 @@ class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
   private var master: String = _
   private var sc: SparkContext = _
   private var fc: FractalContext = _
+
+  private val SIGMA = 1
+  private val DELTA = 0.05
 
   /** set up spark context */
   override def beforeAll: Unit = {
@@ -39,22 +44,33 @@ class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
     Source.fromFile(path).getLines().toList.length
   }
 
+  def wracc(v: VertexInducedSubgraph, c: Computation[VertexInducedSubgraph]): Boolean = {
+    val vertices = v.getVertices
+
+    if (vertices.getSize < SIGMA) {
+      return false
+    }
+
+    true
+  }
+
   test("[exceptionalMining]", Tag("exceptionalMining")) {
+    val graphClass = "br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining"
     val filePath = "../data/exceptionalMining-v1/candidates_1_25_61_62_272.graph"
     val fileLines: Int = getFileLines(filePath)
     val fgraphExceptionalMining: FractalGraph = fc.textFile(filePath)
 
+//    var subgraphs = List()
+
     for (k <- 1 to fileLines) {
-      val graphClass = "br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining"
-      var frac = fgraphExceptionalMining.vfractoid
+      var frac = fgraphExceptionalMining.vfractoid.set("input_graph_class", graphClass)
       for (j <- 1 to k) {
         frac = frac.expand(1)
       }
-      frac.set ("input_graph_class", graphClass)
+      frac = frac.filter(wracc)
 
-      assert(true)
+      assert(frac.subgraphs != null)
     }
-
   }
 
 }
