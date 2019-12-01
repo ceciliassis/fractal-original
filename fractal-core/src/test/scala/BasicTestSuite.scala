7de37f26c3cade6c622234d0aa0fb9db64dfed23
Cecília Assis <cy_assis@hotmail.com>
2019-12-01 04:10:22 -0300

    Remove unused test results

--- fractal-core/src/test/scala/BasicTestSuite.scala
+++ fractal-core/src/test/scala/BasicTestSuite.scala
@@ -13,8 +13,6 @@ class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
   private var master: String = _
   private var sc: SparkContext = _
   private var fc: FractalContext = _
-  private var fgraph: FractalGraph = _
-  private var fgraphEdgeLabel: FractalGraph = _
 
   /** set up spark context */
   override def beforeAll: Unit = {
@@ -27,12 +25,6 @@ class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
     sc = new SparkContext(conf)
     sc.setLogLevel(logLevel)
     fc = new FractalContext(sc, logLevel)
-
-    fgraph = fc.textFile ("../data/cube.graph")
-
-    fgraphEdgeLabel = fc.textFile ("../data/cube-edge-label.graph")
-
-
   }
 
   /** stop spark context */
@@ -42,123 +34,6 @@ class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
       fc.stop()
     }
   }
-
-//  test ("[cube,motifs]", Tag("cube.motifs")) {
-//    // Test output for motifs for subgraph with size 0 to 3
-//
-//    // Expected output
-//    val numSubgraph = List(8, 12, 24)
-//
-//    for (k <- 0 to (numSubgraph.size - 1)) {
-//      val motifsRes = fgraph.motifs.
-//        set ("num_partitions", numPartitions).
-//        explore(k)
-//      val subgraphs = motifsRes.subgraphs
-//
-//      assert(subgraphs.count() == numSubgraph(k))
-//    }
-//
-//  }
-//
-//  test ("[cube,cliques]", Tag("cube.cliques")) {
-//    val numSubgraph = List(8, 12, 0)
-//
-//    for (k <- 0 to (numSubgraph.size - 1)) {
-//      val cliqueRes = fgraph.cliques.
-//        set ("num_partitions", numPartitions).
-//        explore(k)
-//
-//      val subgraphs = cliqueRes.subgraphs
-//      assert(subgraphs.count == numSubgraph(k))
-//    }
-//  }
-//
-//  test ("[cube,cliquesopt]", Tag("cube.cliquesopt")) {
-//    val numSubgraph = List(8, 12, 0)
-//
-//    for (k <- 0 to (numSubgraph.size - 1)) {
-//      val cliqueRes = fgraph.cliquesKClist(k+1).
-//        set ("num_partitions", numPartitions).
-//        explore(k)
-//
-//      val subgraphs = cliqueRes.subgraphs
-//      assert(subgraphs.count == numSubgraph(k))
-//    }
-//  }
-//
-//  test ("[cube,fsm]", Tag("cube.fsm")) {
-//    import br.ufmg.cs.systems.fractal.gmlib.fsm.DomainSupport
-//    import br.ufmg.cs.systems.fractal.pattern.Pattern
-//
-//    // Critical test
-//    // Test output for fsm with support 2 for Subgraphs with size 2 to 3
-//    val support = 2
-//
-//    // Expected output
-//    val numFreqPatterns = List(3, 3+4, 3+4+7)
-//
-//    for (k <- 0 to (numFreqPatterns.size - 1)) {
-//      val fsmRes = fgraph.fsm(support, k).
-//        set ("num_partitions", numPartitions)
-//
-//      val freqPatterns = fsmRes.
-//        aggregationMap [Pattern,DomainSupport] ("frequent_patterns")
-//
-//      assert(freqPatterns.size == numFreqPatterns(k))
-//    }
-//  }
-//
-//  test ("[cube,gquerying]", Tag("cube.gquerying")) {
-//    // Expected output
-//    val numSubgraph = Map("triangles" -> 0, "squares" -> 2)
-//
-//    // triangles
-//    val triangle = new FractalGraph("../data/triangle-test.graph", fgraph.fractalContext)
-//    val triangles = fgraph.gquerying(triangle).
-//      set ("num_partitions", numPartitions).
-//      explore(2)
-//    assert(triangles.numValidSubgraphs() == numSubgraph("triangles"))
-//
-//    // squares
-//    val square = new FractalGraph("../data/square-test.graph", fgraph.fractalContext)
-//    val squares = fgraph.gquerying(square).
-//      set ("num_partitions", numPartitions).
-//      explore(3)
-//    assert (squares.numValidSubgraphs() == numSubgraph("squares"))
-//
-//  }
-//
-//  test ("[cube,vfilter]", Tag("cube.vfilter")) {
-//    val numSubgraph = List(3)
-//    for (k <- 0 to (numSubgraph.size - 1)) {
-//      val frac = fgraph.vfractoidAndExpand.
-//        vfilter [String] (v => v.getVertexLabel() == 1).
-//        set ("num_partitions", numPartitions)
-//      val subgraphs = frac.subgraphs
-//      assert(subgraphs.count == numSubgraph(k))
-//    }
-//  }
-//
-//  test ("[cube,efilter]", Tag("cube.efilter")) {
-//    val numSubgraph = List(2)
-//    for (k <- 0 to (numSubgraph.size - 1)) {
-//      val frac = fgraph.vfractoid.
-//        expand(1).
-//        efilter [String] (e => e.getSourceId() == 1).
-//        expand(1).
-//        set ("num_partitions", numPartitions)
-//      val subgraphs = frac.subgraphs
-//      assert(subgraphs.count == numSubgraph(k))
-//    }
-//  }
-//
-//  test ("[cube,kws]", Tag("cube.kws")) {
-//    val keywords = Array("a", "b")
-//    val kws = fgraphEdgeLabel.keywordSearch(numPartitions, keywords)
-//
-//    assert (kws.subgraphs.count == 2)
-//  }
-
 
   def getFileLines(path: String): Int = {
     Source.fromFile(path).getLines().toList.length
