edbc07cc688b258cf27b77ab5c1ce6985ef8fa3f
Cecília Assis <cy_assis@hotmail.com>
2019-12-01 19:40:05 -0300

    Load main graph

--- fractal-core/src/test/scala/BasicTestSuite.scala
+++ fractal-core/src/test/scala/BasicTestSuite.scala
@@ -1,6 +1,8 @@
 package br.ufmg.cs.systems.fractal
 
 import br.ufmg.cs.systems.fractal.computation.Computation
+import br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining
+import br.ufmg.cs.systems.fractal.graph.{BasicMainGraph, MainGraph}
 import br.ufmg.cs.systems.fractal.subgraph.{EdgeInducedSubgraph, VertexInducedSubgraph}
 import br.ufmg.cs.systems.fractal.util.collection.IntArrayList
 import org.apache.spark.{SparkConf, SparkContext}
@@ -17,21 +19,28 @@ class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
   private var master: String = _
   private var sc: SparkContext = _
   private var fc: FractalContext = _
+  private val exceptionalGraphClass = "br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining"
+  private var exceptionalMiningGraph: ExceptionalMining =_
 
   private val SIGMA = 1
   private val DELTA = 0.05
+
+  private var conf:SparkConf = _
+
 
   /** set up spark context */
   override def beforeAll: Unit = {
     master = s"local[${numPartitions}]"
     // spark conf and context
-    val conf = new SparkConf().
+    conf = new SparkConf().
       setMaster(master).
       setAppName(appName)
 
     sc = new SparkContext(conf)
     sc.setLogLevel(logLevel)
     fc = new FractalContext(sc, logLevel)
+
+    exceptionalMiningGraph = loadExceptionalMainGraph
   }
 
   /** stop spark context */
@@ -42,61 +51,72 @@ class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
     }
   }
 
+  def loadExceptionalMainGraph(): ExceptionalMining = {
+    val path = "../data/exceptionalMining-v1/NYCFoursquareGraph.graph"
+    val graph = fc.textFile(path).vfractoid
+      .set("input_graph_class", exceptionalGraphClass)
+      .expand(1)
+    graph.compute()
+    graph.config.getMainGraph.asInstanceOf[ExceptionalMining]
+  }
+
   def getFileLines(path: String): Int = {
     Source.fromFile(path).getLines().toList.length
   }
 
-//  def gain(vProps, )
+  def gain(vis: VertexInducedSubgraph): Unit = {
+    var v = vis.vertex(0)
+    var vProps: IntArrayList = v.getProperty
+
+    val posAttsLen: Int = vProps.get(0)
+    val negAttsLen: Int = vProps.get(posAttsLen + 1)
+
+    var posAtts = ListBuffer[Int]()
+    var negAtts = ListBuffer[Int]()
+
+    var start = 1
+    var end = posAttsLen
+    for (i <- start to end){
+      posAtts += vProps.get(i)
+    }
+
+    start = end + 2
+    end = start + negAttsLen - 1
+    for (i <- start to end){
+      negAtts += vProps.get(i)
+    }
+
+    var posAttsTot = 0
+    var negAttsTot = 0
+    var attsTot = 0
+
+    end += 2
+
+    for (i <- 0 to vis.getNumVertices - 1) {
+      v = vis.vertex(i)
+      vProps = v.getProperty
+
+      posAtts.foreach((idx) => posAttsTot += vProps.get(end + idx))
+      negAtts.foreach((idx) => negAttsTot += vProps.get(end + idx))
+      attsTot += vProps.getLast
+    }
+  }
+
+
 
   test("[exceptionalMining]", Tag("exceptionalMining")) {
-    val graphClass = "br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining"
     var filePath = "../data/exceptionalMining-v1/candidates_1_25_61_62_272.graph"
-    filePath = "../data/exceptionalMining-v1/candidates_4_34_249_271.graph"
+//    filePath = "../data/exceptionalMining-v1/candidates_4_34_249_271.graph"
     val fileLines: Int = getFileLines(filePath)
     val fgraphExceptionalMining: FractalGraph = fc.textFile(filePath)
 
-
     val wracc = (vis: VertexInducedSubgraph, cvis: Computation[VertexInducedSubgraph]) => {
-      var v = vis.vertex(0)
-      var vProps: IntArrayList = v.getProperty
-
-      val posAttsLen: Int = vProps.get(0)
-      val negAttsLen: Int = vProps.get(posAttsLen + 1)
-
-      var posAtts = ListBuffer[Int]()
-      var negAtts = ListBuffer[Int]()
-
-      var start = 1
-      var end = posAttsLen
-      for (i <- start to end){
-        posAtts += vProps.get(i)
-      }
-
-      start = end + 2
-      end = start + negAttsLen - 1
-      for (i <- start to end){
-        negAtts += vProps.get(i)
-      }
-
-      var posAttsTot = 0
-      var negAttsTot = 0
-      var attsTot = 0
-
-      end += 2
-
-      for (i <- 0 to vis.getNumVertices - 1) {
-        v = vis.vertex(i)
-        vProps = v.getProperty
-
-        posAtts.foreach((idx) => posAttsTot += vProps.get(end + idx))
-        negAtts.foreach((idx) => negAttsTot += vProps.get(end + idx))
-        attsTot += vProps.getLast
-      }
+      gain(vis)
       false
     }
 
     for (k <- 1 to fileLines) {
-      var frac = fgraphExceptionalMining.vfractoid.set("input_graph_class", graphClass)
+      var frac = fgraphExceptionalMining.vfractoid.set("input_graph_class", exceptionalGraphClass)
 
       for (j <- 1 to k) {
         frac = frac.expand(1)
