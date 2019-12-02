1a9a4f97c6f026946492e16ff1cfd2a66d456ce1
Cecília Assis <cy_assis@hotmail.com>
2019-12-02 00:48:46 -0300

    Add default tests

--- fractal-core/src/test/scala/BasicTestSuite.scala
+++ fractal-core/src/test/scala/BasicTestSuite.scala
@@ -1,15 +1,7 @@
 package br.ufmg.cs.systems.fractal
 
-import br.ufmg.cs.systems.fractal.computation.Computation
-import br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining
-import br.ufmg.cs.systems.fractal.graph.{BasicMainGraph, MainGraph}
-import br.ufmg.cs.systems.fractal.subgraph.{EdgeInducedSubgraph, VertexInducedSubgraph}
-import br.ufmg.cs.systems.fractal.util.collection.IntArrayList
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Tag}
-
-import scala.collection.mutable.ListBuffer
-import scala.io.Source
 
 class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
   private val numPartitions: Int = 8
@@ -19,20 +11,14 @@ class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
   private var master: String = _
   private var sc: SparkContext = _
   private var fc: FractalContext = _
-  private val exceptionalGraphClass = "br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining"
-  private var exceptionalMiningGraph: ExceptionalMining =_
-
-  private val SIGMA = 1
-  private val DELTA = 0.05
-
-  private var conf:SparkConf = _
-
+  private var fgraph: FractalGraph = _
+  private var fgraphEdgeLabel: FractalGraph = _
 
   /** set up spark context */
   override def beforeAll: Unit = {
     master = s"local[${numPartitions}]"
     // spark conf and context
-    conf = new SparkConf().
+    val conf = new SparkConf().
       setMaster(master).
       setAppName(appName)
 
@@ -40,7 +26,9 @@ class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
     sc.setLogLevel(logLevel)
     fc = new FractalContext(sc, logLevel)
 
-    exceptionalMiningGraph = loadExceptionalMainGraph
+    fgraph = fc.textFile("../data/cube.graph")
+
+    fgraphEdgeLabel = fc.textFile("../data/cube-edge-label.graph")
   }
 
   /** stop spark context */
@@ -51,82 +39,119 @@ class BasicTestSuite extends FunSuite with BeforeAndAfterAll {
     }
   }
 
-  def loadExceptionalMainGraph(): ExceptionalMining = {
-    val path = "../data/exceptionalMining-v1/NYCFoursquareGraph.graph"
-    val graph = fc.textFile(path).vfractoid
-      .set("input_graph_class", exceptionalGraphClass)
-      .expand(1)
-    graph.compute()
-    graph.config.getMainGraph.asInstanceOf[ExceptionalMining]
-  }
+  test("[cube,motifs]", Tag("cube.motifs")) {
+    // Test output for motifs for subgraph with size 0 to 3
 
-  def getFileLines(path: String): Int = {
-    Source.fromFile(path).getLines().toList.length
-  }
+    // Expected output
+    val numSubgraph = List(8, 12, 24)
 
-  def gain(vis: VertexInducedSubgraph): Unit = {
-    var v = vis.vertex(0)
-    var vProps: IntArrayList = v.getProperty
+    for (k <- 0 to (numSubgraph.size - 1)) {
+      val motifsRes = fgraph.motifs.
+        set("num_partitions", numPartitions).
+        explore(k)
+      val subgraphs = motifsRes.subgraphs
 
-    val posAttsLen: Int = vProps.get(0)
-    val negAttsLen: Int = vProps.get(posAttsLen + 1)
-
-    var posAtts = ListBuffer[Int]()
-    var negAtts = ListBuffer[Int]()
-
-    var start = 1
-    var end = posAttsLen
-    for (i <- start to end){
-      posAtts += vProps.get(i)
+      assert(subgraphs.count() == numSubgraph(k))
     }
 
-    start = end + 2
-    end = start + negAttsLen - 1
-    for (i <- start to end){
-      negAtts += vProps.get(i)
-    }
+  }
 
-    var posAttsTot = 0
-    var negAttsTot = 0
-    var attsTot = 0
+  test("[cube,cliques]", Tag("cube.cliques")) {
+    val numSubgraph = List(8, 12, 0)
 
-    end += 2
+    for (k <- 0 to (numSubgraph.size - 1)) {
+      val cliqueRes = fgraph.cliques.
+        set("num_partitions", numPartitions).
+        explore(k)
 
-    for (i <- 0 to vis.getNumVertices - 1) {
-      v = vis.vertex(i)
-      vProps = v.getProperty
-
-      posAtts.foreach((idx) => posAttsTot += vProps.get(end + idx))
-      negAtts.foreach((idx) => negAttsTot += vProps.get(end + idx))
-      attsTot += vProps.getLast
+      val subgraphs = cliqueRes.subgraphs
+      assert(subgraphs.count == numSubgraph(k))
     }
   }
 
+  test("[cube,cliquesopt]", Tag("cube.cliquesopt")) {
+    val numSubgraph = List(8, 12, 0)
 
+    for (k <- 0 to (numSubgraph.size - 1)) {
+      val cliqueRes = fgraph.cliquesKClist(k + 1).
+        set("num_partitions", numPartitions).
+        explore(k)
 
-  test("[exceptionalMining]", Tag("exceptionalMining")) {
-    var filePath = "../data/exceptionalMining-v1/candidates_1_25_61_62_272.graph"
-//    filePath = "../data/exceptionalMining-v1/candidates_4_34_249_271.graph"
-    val fileLines: Int = getFileLines(filePath)
-    val fgraphExceptionalMining: FractalGraph = fc.textFile(filePath)
-
-    val wracc = (vis: VertexInducedSubgraph, cvis: Computation[VertexInducedSubgraph]) => {
-      gain(vis)
-      false
-    }
-
-    for (k <- 1 to fileLines) {
-      var frac = fgraphExceptionalMining.vfractoid.set("input_graph_class", exceptionalGraphClass)
-
-      for (j <- 1 to k) {
-        frac = frac.expand(1)
-      }
-
-      frac = frac.filter(wracc)
-//      frac = frac.filter((e,c) => false)
-
-      assert(frac.subgraphs != null)
+      val subgraphs = cliqueRes.subgraphs
+      assert(subgraphs.count == numSubgraph(k))
     }
   }
 
+  test("[cube,fsm]", Tag("cube.fsm")) {
+    import br.ufmg.cs.systems.fractal.gmlib.fsm.DomainSupport
+    import br.ufmg.cs.systems.fractal.pattern.Pattern
+
+    // Critical test
+    // Test output for fsm with support 2 for Subgraphs with size 2 to 3
+    val support = 2
+
+    // Expected output
+    val numFreqPatterns = List(3, 3 + 4, 3 + 4 + 7)
+
+    for (k <- 0 to (numFreqPatterns.size - 1)) {
+      val fsmRes = fgraph.fsm(support, k).
+        set("num_partitions", numPartitions)
+
+      val freqPatterns = fsmRes.
+        aggregationMap[Pattern, DomainSupport]("frequent_patterns")
+
+      assert(freqPatterns.size == numFreqPatterns(k))
+    }
+  }
+
+  test("[cube,gquerying]", Tag("cube.gquerying")) {
+    // Expected output
+    val numSubgraph = Map("triangles" -> 0, "squares" -> 2)
+
+    // triangles
+    val triangle = new FractalGraph("../data/triangle-test.graph", fgraph.fractalContext)
+    val triangles = fgraph.gquerying(triangle).
+      set("num_partitions", numPartitions).
+      explore(2)
+    assert(triangles.numValidSubgraphs() == numSubgraph("triangles"))
+
+    // squares
+    val square = new FractalGraph("../data/square-test.graph", fgraph.fractalContext)
+    val squares = fgraph.gquerying(square).
+      set("num_partitions", numPartitions).
+      explore(3)
+    assert(squares.numValidSubgraphs() == numSubgraph("squares"))
+
+  }
+
+  test("[cube,vfilter]", Tag("cube.vfilter")) {
+    val numSubgraph = List(3)
+    for (k <- 0 to (numSubgraph.size - 1)) {
+      val frac = fgraph.vfractoidAndExpand.
+        vfilter[String](v => v.getVertexLabel() == 1).
+        set("num_partitions", numPartitions)
+      val subgraphs = frac.subgraphs
+      assert(subgraphs.count == numSubgraph(k))
+    }
+  }
+
+  test("[cube,efilter]", Tag("cube.efilter")) {
+    val numSubgraph = List(2)
+    for (k <- 0 to (numSubgraph.size - 1)) {
+      val frac = fgraph.vfractoid.
+        expand(1).
+        efilter[String](e => e.getSourceId() == 1).
+        expand(1).
+        set("num_partitions", numPartitions)
+      val subgraphs = frac.subgraphs
+      assert(subgraphs.count == numSubgraph(k))
+    }
+  }
+
+  test("[cube,kws]", Tag("cube.kws")) {
+    val keywords = Array("a", "b")
+    val kws = fgraphEdgeLabel.keywordSearch(numPartitions, keywords)
+
+    assert(kws.subgraphs.count == 2)
+  }
 }
