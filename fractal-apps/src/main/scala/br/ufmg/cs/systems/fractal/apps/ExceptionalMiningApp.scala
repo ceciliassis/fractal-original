8354976a3c8427749a25371ae3dd9e1fe47beb82
Cecília Assis <cy_assis@hotmail.com>
2019-12-02 05:53:15 -0300

    Read files from folder

--- fractal-apps/src/main/scala/br/ufmg/cs/systems/fractal/apps/ExceptionalMiningApp.scala
+++ fractal-apps/src/main/scala/br/ufmg/cs/systems/fractal/apps/ExceptionalMiningApp.scala
@@ -1,6 +1,8 @@
 package br.ufmg.cs.systems.fractal.apps
 
-import br.ufmg.cs.systems.fractal._
+import java.io.File
+
+import br.ufmg.cs.systems.fractal.{FractalGraph, _}
 import br.ufmg.cs.systems.fractal.computation.Computation
 import br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining
 import br.ufmg.cs.systems.fractal.subgraph.{ResultSubgraph, VertexInducedSubgraph}
@@ -32,7 +34,7 @@ object ExceptionalMiningApp extends Logging {
 
     //  [ENERGETICS] Graph init
     val exceptionalGraphClass = "br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining"
-    var graphPath = "data/exceptionalMining-v1/NYCFoursquareGraph.graph"
+    val graphPath = "data/exceptionalMining-v1/main.graph"
 
     val loadExceptionalMainGraph: ExceptionalMining = {
       val graph = fc.textFile(graphPath).vfractoid
@@ -150,25 +152,39 @@ object ExceptionalMiningApp extends Logging {
 
     }
 
-    //  RUN
-//    TODO: read graps folder
-    graphPath = "data/exceptionalMining-v1/candidates_1_25_61_62_272.graph"
 
-    val fileLines: Int = getFileLines(graphPath)
-    val fgraph = fc.textFile(graphPath)
+    val getFiles = (dirPath: String) => {
+      new File(dirPath).listFiles().filter(_.getName.endsWith(".graph"))
+    }
+
+
+    //  RUN
+    //    TODO: read graps folder
+    val dirPath = "data/exceptionalMining-v1/candidates"
+    val files = getFiles(dirPath)
+
     var subgraphs = new ListBuffer[RDD[ResultSubgraph[_]]]
 
+    var fileLines = 0
+    var fGraph: FractalGraph = null
+    var filePath = ""
 
-    val run: Int => Fractoid[VertexInducedSubgraph] = (k) => {
-      var frac = fgraph.vfractoid.set("input_graph_class", exceptionalGraphClass)
+    val expanded: (Int, FractalGraph) => Fractoid[VertexInducedSubgraph] = (k, fGraph) => {
+      var frac = fGraph.vfractoid.set("input_graph_class", exceptionalGraphClass)
       for (_ <- 1 to k) {
         frac = frac.expand(1)
       }
       frac
     }
 
-    for (k <- 1 to fileLines) {
-      subgraphs += run(k).filter(wracc).subgraphs
+    files.foreach {
+      file =>
+        filePath = file.getPath
+        fGraph = fc.textFile(filePath)
+        fileLines = getFileLines(filePath)
+        for (k <- 1 to fileLines) {
+          subgraphs += expanded(k, fGraph).filter(wracc).subgraphs
+        }
     }
 
     // ENV CLEANING
