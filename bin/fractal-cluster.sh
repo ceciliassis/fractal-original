c53161ea799293cf820c333c268c72fa53f68ddd
Cecília Assis <cy_assis@hotmail.com>
2020-01-13 20:38:08 -0300

    Add rebuild automation script

---
+++ bin/fractal-cluster.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [ -z $FRACTAL_HOME ]; then
+	echo "FRACTAL_HOME is unset"
+	exit 1
+else
+	echo "FRACTAL_HOME is set to $FRACTAL_HOME"
+fi
+
+if [ -z $SPARK_HOME ]; then
+	echo "SPARK_HOME is unset"
+	exit 1
+else
+	echo "SPARK_HOME is set to $SPARK_HOME"
+fi
+
+if [ -z $SPARK_HOME ]; then
+	echo "SPARK_HOME is unset"
+	exit 1
+else
+	echo "SPARK_HOME is set to $SPARK_HOME"
+fi
+
+export SPARK_SUBMIT_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
+
+bash -c "./gradlew assemble"
+bash -c "hdfs dfs -put fractal-core/build/libs/fractal-core-SPARK-2.2.0.jar /user/ceciliassis/"
+bash -c "hdfs dfs -put fractal-apps/build/libs/fractal-apps-SPARK-2.2.0.jar /user/ceciliassis/"
