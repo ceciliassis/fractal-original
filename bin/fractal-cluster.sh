cc023551ea227d6c1804ee5d9288d366b861af2e
Cecília Assis <cy_assis@hotmail.com>
2020-01-13 20:43:41 -0300

    Log cluster bin steps

--- bin/fractal-cluster.sh
+++ bin/fractal-cluster.sh
@@ -14,8 +14,12 @@ else
 	echo "SPARK_HOME is set to $SPARK_HOME"
 fi
 
+echo "Setting SPARK_SUBMIT_OPTS to -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005"
 export SPARK_SUBMIT_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
 
+echo "Running gradlew"
 bash -c "./gradlew assemble"
+
+echo "Updating fractal-core and fractal-apps JARS on HDFS"
 bash -c "hdfs dfs -put -f fractal-core/build/libs/fractal-core-SPARK-2.2.0.jar /user/ceciliassis/"
 bash -c "hdfs dfs -put -f fractal-apps/build/libs/fractal-apps-SPARK-2.2.0.jar /user/ceciliassis/"
