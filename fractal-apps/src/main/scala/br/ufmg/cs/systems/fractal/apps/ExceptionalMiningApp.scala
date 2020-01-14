d996c0ee11183d8edd2d525ae3b60f12406282ea
Cecília Assis <cy_assis@hotmail.com>
2020-01-13 22:12:14 -0300

    Get candidates files with hdfs

--- fractal-apps/src/main/scala/br/ufmg/cs/systems/fractal/apps/ExceptionalMiningApp.scala
+++ fractal-apps/src/main/scala/br/ufmg/cs/systems/fractal/apps/ExceptionalMiningApp.scala
@@ -8,7 +8,6 @@ import br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining
 import br.ufmg.cs.systems.fractal.subgraph.{ResultSubgraph, VertexInducedSubgraph}
 import br.ufmg.cs.systems.fractal.util.Logging
 import br.ufmg.cs.systems.fractal.util.collection.IntArrayList
-import org.apache.commons.httpclient.URI
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.rdd.RDD
@@ -19,25 +18,36 @@ import scala.io.Source
 
 
 object ExceptionalMiningApp extends Logging {
+  def getFileLines(path: String): Int = {
+    Source.fromFile(path).getLines().toList.length
+  }
+
   def main(args: Array[String]): Unit = {
     // environment setup
     //    val conf = new SparkConf().setMaster(s"local[*]").setAppName("ExceptionalMiningApp")
     val conf = new SparkConf().setAppName("ExceptionalMiningApp")
     val sc = new SparkContext(conf)
     val fc = new FractalContext(sc)
-    val hdfsFileSystem = FileSystem.get(new Configuration())
 
+    val hdfsFileSystem = FileSystem.get(new Configuration())
+    val hdfsCore = "hdfs://compute1:9000"
+
+    val userFolder = "user/ceciliassis"
 
     //  ENERGETICS
     val SIGMA = 1
     val DELTA = 0.05
 
     //  [ENERGETICS] Graph init
-    val fractalDatasets = "hdfs://compute1:9000/user/ceciliassis/fractal/data/exceptionalMining-v1"
-    val graphClass = "br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining"
+    val fractalDatasets = s"${hdfsCore}/${userFolder}/fractal/data/exceptionalMining-v1"
 
+    //  RUN
+    val candidatesFiles = hdfsFileSystem.globStatus(new Path(s"${fractalDatasets}/candidates/*.graph"))
 
-
+    candidatesFiles.foreach {
+      file =>
+        print(file)
+    }
 
     // ENV CLEANING
     fc.stop()
