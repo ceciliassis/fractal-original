e65792791f62323d224a7a6661e4d2e5fb20ca57
Cecília Assis <cy_assis@hotmail.com>
2020-02-23 02:01:53 -0300

    Fix Set input graph before expand

--- fractal-apps/src/main/scala/br/ufmg/cs/systems/fractal/apps/ExceptionalMiningApp.scala
+++ fractal-apps/src/main/scala/br/ufmg/cs/systems/fractal/apps/ExceptionalMiningApp.scala
@@ -39,7 +39,10 @@ object ExceptionalMiningApp extends Logging {
     val graphClass = "br.ufmg.cs.systems.fractal.gmlib.exceptionalmining.ExceptionalMining"
     val graphPath = s"${fractalDatasets}/maingraph/main.graph"
 
-    val mainGraph = fc.textFile(graphPath).vfractoid.expand(1).set("input_graph", graphClass)
+    val mainGraph = fc.textFile(graphPath)
+                      .vfractoid
+                      .expand(1)
+                      .set("input_graph_class", graphClass)
     val subgraphs = mainGraph.subgraphs
 
     val exceptionalMainGraph = mainGraph.config.getMainGraph.asInstanceOf[ExceptionalMining]
